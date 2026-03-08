ee7fca6bb3223eaad1c89987b4db37624cf28416
tunzy-shop <tunzyprivate@gmail.com>
2026-03-08 02:20:35 +0100

    Update commands.js

--- api/commands.js
+++ api/commands.js
@@ -1,1 +1,31 @@
+const commandHandler = require('../lib/commandHandler');
+const newsletterUtils = require('../lib/newsletter');
 
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    const commands = commandHandler.getAllCommands().map(cmd => ({
+        name: cmd.name,
+        category: cmd.category,
+        description: cmd.description
+    }));
+    
+    // Group by category
+    const categories = {};
+    commands.forEach(cmd => {
+        if (!categories[cmd.category]) {
+            categories[cmd.category] = [];
+        }
+        categories[cmd.category].push(cmd);
+    });
+    
+    res.json({
+        success: true,
+        total: commands.length,
+        categories: Object.keys(categories),
+        commands: commands,
+        grouped: categories,
+        newsletter: newsletterUtils.getNewsletterName(),
+        prefix: process.env.PREFIX
+    });
+};
