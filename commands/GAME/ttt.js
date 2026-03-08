22799d65e21bf8075f7b5cbce7c1f184410be34a
tunzy-shop <tunzyprivate@gmail.com>
2026-03-08 06:01:03 +0100

    Update ttt.js

--- commands/GAME/ttt.js
+++ commands/GAME/ttt.js
@@ -1,13 +1,42 @@
-const newsletterUtils = require('../../lib/newsletter');
+// Simple Tic Tac Toe game
+let games = {};
 
 module.exports = {
     name: 'ttt',
     category: 'GAME',
     description: 'Play Tic Tac Toe',
     execute: async (message, args, negga) => {
-        await negga.sendMessage(message.from, 
-            `_Tic Tac Toe game started_`, 
-            newsletterUtils.getNormalOptions()
-        );
+        const chatId = message.from;
+        
+        if (games[chatId]) {
+            return message.reply('_A game is already in progress_');
+        }
+        
+        // Initialize game
+        games[chatId] = {
+            board: ['⬜', '⬜', '⬜', '⬜', '⬜', '⬜', '⬜', '⬜', '⬜'],
+            turn: 'X',
+            players: [message.author],
+            moves: 0
+        };
+        
+        const board = games[chatId].board;
+        const display = `
+_Current board:_
+${board[0]} ${board[1]} ${board[2]}
+${board[3]} ${board[4]} ${board[5]}
+${board[6]} ${board[7]} ${board[8]}
+
+_Turn: Player ${games[chatId].turn}_
+_Use .ttt 1-9 to place your mark_`;
+        
+        await message.reply(display);
+        
+        // Game timeout after 5 minutes
+        setTimeout(() => {
+            if (games[chatId]) {
+                delete games[chatId];
+            }
+        }, 300000);
     }
 };
