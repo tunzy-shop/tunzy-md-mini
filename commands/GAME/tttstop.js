6a41fc9fb102ec91b3195ec9c475262156d842cd
tunzy-shop <tunzyprivate@gmail.com>
2026-03-08 06:01:24 +0100

    Update tttstop.js

--- commands/GAME/tttstop.js
+++ commands/GAME/tttstop.js
@@ -1,13 +1,17 @@
-const newsletterUtils = require('../../lib/newsletter');
+let games = {};
 
 module.exports = {
     name: 'tttstop',
     category: 'GAME',
     description: 'Stop Tic Tac Toe',
     execute: async (message, args, negga) => {
-        await negga.sendMessage(message.from, 
-            `_Tic Tac Toe game stopped_`, 
-            newsletterUtils.getNormalOptions()
-        );
+        const chatId = message.from;
+        
+        if (games[chatId]) {
+            delete games[chatId];
+            await message.reply('_Game stopped_');
+        } else {
+            await message.reply('_No game in progress_');
+        }
     }
 };
