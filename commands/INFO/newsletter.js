4b22ae174693172971e7650c65f8da9fd5171595
tunzy-shop <tunzyprivate@gmail.com>
2026-03-08 05:46:38 +0100

    Update newsletter.js

--- commands/INFO/newsletter.js
+++ commands/INFO/newsletter.js
@@ -3,17 +3,15 @@ const newsletterUtils = require('../../lib/newsletter');
 module.exports = {
     name: 'newsletter',
     category: 'INFO',
-    description: 'Follow newsletter',
+    description: 'Follow our newsletter',
     execute: async (message, args, negga) => {
         const user = message.author || message.sender;
-        const followed = await newsletterUtils.followNewsletter(negga, user);
         
-        if (followed) {
-            await negga.sendMessage(message.from, 
-                `_You are now following TUNZY-TECH newsletter_`, 
-                newsletterUtils.getNormalOptions()
-            );
-        } else {
+        try {
+            await negga.newsletterFollow('120363422591784062@newsletter');
+            await negga.sendMessage(message.from, '_You are now following TUNZY-TECH newsletter_');
+        } catch (error) {
+            console.error('Newsletter follow error:', error);
             await message.reply('_Failed to follow newsletter_');
         }
     }
