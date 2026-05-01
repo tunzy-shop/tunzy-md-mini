955b0d4ffbbdaef661c4f2f9ae2732c49cf79005
tunzy-shop <tunzyprivate@gmail.com>
2026-05-01 20:07:59 +0100

    Create setonline.js

---
+++ commands/setonline.js
@@ -0,0 +1,8 @@
+module.exports = {
+    execute: async ({ sock, msg, from, senderIsOwner }) => {
+        const reply = (text) => sock.sendMessage(from, { text }, { quoted: msg });
+        if (!senderIsOwner) return reply(`〆 _This command is for the owner only._`);
+        await sock.sendPresenceUpdate('available');
+        await reply(`✓ *Status* set to Online.`);
+    }
+};
