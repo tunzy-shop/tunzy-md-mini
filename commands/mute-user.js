a3dc1f43dcc6c5d0bffc2858ee4237ba1ae326cc
tunzy-shop <tunzyprivate@gmail.com>
2026-05-01 19:59:59 +0100

    Create mute-user.js

---
+++ commands/mute-user.js
@@ -0,0 +1,9 @@
+module.exports = {
+    execute: async ({ sock, msg, from, isGroup }) => {
+        const reply = (text) => sock.sendMessage(from, { text }, { quoted: msg });
+        if (!isGroup) return reply(`〆 _This command is for groups only._`);
+        const target = msg.message?.extendedTextMessage?.contextInfo?.participant;
+        if (!target) return reply(`〆 _Reply to a user's message._`);
+        await reply(`〆 _Mute user coming soon!_`);
+    }
+};
