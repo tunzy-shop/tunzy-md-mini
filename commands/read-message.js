1fb1169cd93a5193d3548fbe0f092dd309292946
tunzy-shop <tunzyprivate@gmail.com>
2026-05-01 20:11:44 +0100

    Create read-message.js

---
+++ commands/read-message.js
@@ -0,0 +1,7 @@
+module.exports = {
+    execute: async ({ sock, msg, from, senderIsOwner }) => {
+        const reply = (text) => sock.sendMessage(from, { text }, { quoted: msg });
+        if (!senderIsOwner) return reply(`〆 _This command is for the owner only._`);
+        await reply(`✓ *Read-Message* toggled!`);
+    }
+};
