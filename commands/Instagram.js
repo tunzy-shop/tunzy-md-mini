97672e816991a203cfbab93d8dd68c3481fbc232
tunzy-shop <tunzyprivate@gmail.com>
2026-05-01 19:54:56 +0100

    Create Instagram.js

---
+++ commands/Instagram.js
@@ -0,0 +1,8 @@
+module.exports = {
+    execute: async ({ sock, msg, from, args, PREFIX }) => {
+        const reply = (text) => sock.sendMessage(from, { text }, { quoted: msg });
+        const url = args[0];
+        if (!url) return reply(`〆 _Usage : ${PREFIX}instagram <url>_`);
+        await reply(`〆 _Instagram downloader coming soon!_`);
+    }
+};
