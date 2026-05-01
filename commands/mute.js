1369630ebcd2409e7d4b2785bcb0d53ea838cca6
tunzy-shop <tunzyprivate@gmail.com>
2026-05-01 19:59:25 +0100

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,12 @@
+const { isAdmin, isBotAdmin } = require('../lib/isAdmin');
+module.exports = {
+    execute: async ({ sock, msg, from, sender, isGroup, senderIsOwner }) => {
+        const reply = (text) => sock.sendMessage(from, { text }, { quoted: msg });
+        if (!isGroup) return reply(`〆 _This command is for groups only._`);
+        const senderIsAdmin = await isAdmin(sock, from, sender);
+        if (!senderIsAdmin && !senderIsOwner) return reply(`〆 _You must be an admin to use this command._`);
+        if (!await isBotAdmin(sock, from)) return reply(`〆 _Bot must be an admin to use this command._`);
+        await sock.groupSettingUpdate(from, 'announcement');
+        await reply(`✓ *Group muted.* _Only admins can send messages._`);
+    }
+};
